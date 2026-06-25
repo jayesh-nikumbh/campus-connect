c341e27179771be9a895b4b17c43b42d3c063e30
jayesh-nikumbh <jnikumbh69@gmail.com>
2026-06-25 16:30:15 +0530

    by

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    watch: {
-      usePolling: true,   // Windows pe HMR fix — file changes detect karne ke liye
-      interval: 100,      // 100ms pe check karo
-    },
-  },
 })
